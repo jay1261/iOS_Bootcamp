aac868aeab7207ce8ef9828ff142ceb7c8463a3c
jay1261 <dongjae73@naver.com>
2022-09-20 15:43:02 +0900

    I am Rich app first commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 문호상 on 2022/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
