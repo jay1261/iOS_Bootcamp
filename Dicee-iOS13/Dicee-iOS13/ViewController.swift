3486501d62ff9e4cf0ea4de5e4d8f99326865c8c
jay1261 <dongjae73@naver.com>
2022-09-30 23:36:50 +0900

    UI image array

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var diceImageView1: UIImageView!
     
+    var leftDiceNumber = 1;
+    var rightDiceNumber = 5;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,6 +25,14 @@ class ViewController: UIViewController {
         diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        if leftDiceNumber > 5 {leftDiceNumber = 0}
+        if rightDiceNumber < 0 {rightDiceNumber = 5}
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"),  #imageLiteral(resourceName: "DiceTwo"),  #imageLiteral(resourceName: "DiceThree"),  #imageLiteral(resourceName: "DiceFour"),  #imageLiteral(resourceName: "DiceFive"),  #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"),  #imageLiteral(resourceName: "DiceTwo"),  #imageLiteral(resourceName: "DiceThree"),  #imageLiteral(resourceName: "DiceFour"),  #imageLiteral(resourceName: "DiceFive"),  #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
     
 }
 
