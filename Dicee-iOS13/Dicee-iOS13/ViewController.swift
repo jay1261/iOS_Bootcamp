a70e25aa488c661d6f2d4d41f6f7a6b720e899fc
jay1261 <dongjae73@naver.com>
2022-10-01 00:33:34 +0900

    Show images randomly

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -13,25 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var diceImageView1: UIImageView!
     
-    var leftDiceNumber = 1;
-    var rightDiceNumber = 5;
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        diceImageView1.image =  UIImage(imageLiteralResourceName: "DiceSix")
-        
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
-    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        if leftDiceNumber > 5 {leftDiceNumber = 0}
-        if rightDiceNumber < 0 {rightDiceNumber = 5}
-        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"),  #imageLiteral(resourceName: "DiceTwo"),  #imageLiteral(resourceName: "DiceThree"),  #imageLiteral(resourceName: "DiceFour"),  #imageLiteral(resourceName: "DiceFive"),  #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
-        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"),  #imageLiteral(resourceName: "DiceTwo"),  #imageLiteral(resourceName: "DiceThree"),  #imageLiteral(resourceName: "DiceFour"),  #imageLiteral(resourceName: "DiceFive"),  #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"),  #imageLiteral(resourceName: "DiceTwo"),  #imageLiteral(resourceName: "DiceThree"),  #imageLiteral(resourceName: "DiceFour"),  #imageLiteral(resourceName: "DiceFive"),  #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+        // 랜덤 1~10까지
+        //print(Int.random(in: 1...10))
     }
     
 }
