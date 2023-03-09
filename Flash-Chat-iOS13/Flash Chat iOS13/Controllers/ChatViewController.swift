1a9ee3cfcf7911a29efa21ea2a0b5d245dd1b950
jay1261 <dongjae73@naver.com>
2023-03-09 23:26:46 +0900

    learn: UITableView

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -13,13 +13,39 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let messages : [Message] = [
+        Message(sender: "1@2.com", body: "Hi"),
+        Message(sender: "1@2.com", body: "Hello~?"),
+        Message(sender: "a@b.com", body: "Yeah")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
     }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = messages[indexPath.row].sender
+            content.secondaryText = messages[indexPath.row].body
+            cell.contentConfiguration = content
+            return cell
+        } else {
+            // Fallback on earlier versions
+            return cell
+        }
+    }
 }
---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Dongjae on 2023/03/09.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender : String
+    let body : String
+}
