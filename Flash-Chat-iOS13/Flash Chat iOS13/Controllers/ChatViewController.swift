dc3d5ad6da754ea934d7298d3ce5c09c514958c9
jay1261 <dongjae73@naver.com>
2023-03-18 00:44:51 +0900

    learn: UI navigationBar, tableView scrollToRow

--- Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -52,6 +52,9 @@ class ChatViewController: UIViewController {
                         
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
+                            
+                            let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                            self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                         }
                         
                     }
@@ -73,6 +76,10 @@ class ChatViewController: UIViewController {
                 }
                 else {
                     print("Successfully saved data")
+                    
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""
+                    }
                 }
             }
         }
@@ -87,12 +94,29 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         if #available(iOS 14.0, *) {
             var content = cell.defaultContentConfiguration()
-            content.text = messages[indexPath.row].body
+            content.text = message.body
 //            content.secondaryText = messages[indexPath.row].body
             cell.label.text = content.text
+            
+            if message.sender == Auth.auth().currentUser?.email {
+                cell.leftImageView.isHidden = true
+                cell.rightImageView.isHidden = false
+                cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+                cell.label.textColor = UIColor(named: K.BrandColors.purple)
+            }
+            else {
+                cell.leftImageView.isHidden = false
+                cell.rightImageView.isHidden = true
+                cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lighBlue)
+                cell.label.textColor = UIColor(named: K.BrandColors.blue)
+            }
+            
+            
             return cell
         } else {
             // Fallback on earlier versions
