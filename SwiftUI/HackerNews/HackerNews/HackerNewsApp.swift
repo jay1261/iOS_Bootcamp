889acceeb43e260dd1af680094c5f21256aeee72
jay1261 <dongjae73@naver.com>
2023-03-23 00:20:42 +0900

    learn: SwiftUI List

---
+++ SwiftUI/HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Jay on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
